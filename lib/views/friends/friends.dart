ac9f104aa5842d788740ed09dbf9e392afc55773
DerMathes <nik279.m@gmail.com>
2020-12-21 18:57:27 +0100

    Friends Name

--- lib/views/friends/friends.dart
+++ lib/views/friends/friends.dart
@@ -1,22 +1,80 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mylife/views/home/homescreen.dart';
 import '../../main.dart';
 
-class Friends extends StatelessWidget {
-  var _controller = TextEditingController();
+class Friends extends StatefulWidget {
+  FriendsPage createState() => FriendsPage();
+}
+
+class FriendsPage extends State<Friends> {
+  final myController = TextEditingController();
+  List<String> friendsName = [];
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Freunde"),
         backgroundColor: HexColor('AD6B07'),
+
       ),
       body: Container(
         decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage("assets/images/background.png"),
                 fit: BoxFit.fill)),
+        child: ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: friendsName.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Container(
+              height: 50,
+              color: HexColor('AD6B07'),
+              child: Center(
+                child: Text('${friendsName[index]}', style: TextStyle(color: Colors.white),),
+              ),
+            );
+          },
+          separatorBuilder: (BuildContext context, int index) => const Divider(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          return showDialog<void>(
+            context: context,
+            barrierDismissible: false, // user must tap button!
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text('Neue/r Freund/in'),
+                content: Row(children: <Widget>[
+                  Expanded(
+                    child: TextFormField(
+                      controller: myController,
+                      decoration: InputDecoration(labelText: 'Name eingeben'),
+                    ),
+                  ),
+                  TextButton(
+                    child: Text('Speichern'),
+                    onPressed: () {
+                      friendsName.add("${myController.text}");
+                      Navigator.of(context).pop();
+                      setState(() {});
+                    },
+                  ),
+                ],
+              ),
+              );
+            },
+          );
+        },
+        child: Icon(Icons.plus_one),
+        backgroundColor: HexColor('AD6B07'),
       ),
     );
   }
